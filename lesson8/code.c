7b08c1ebed6e39cf0d7cacecbd524ad1d6f7465d
是回忆加了分 <2144846840@qq.com>
2024-09-19 15:23:21 +0800

    debug learning

--- lesson8/code.c
+++ lesson8/code.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+int Add(int a,int b)
+{
+	return a+b;
+}
+
+
 int main()
 {
 	printf("hello world\n");
+	int ret=Add(10,20);
+	printf("%d\n",ret);
 	printf("hello linux\n");
 	
 	return 0;
