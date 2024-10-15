9361ea8ad5b8639e04aa1ad2c5bd28b618512405
是回忆加了分 <2144846840@qq.com>
2024-10-15 14:48:27 +0800

    error学习

--- lesson11/code.c
+++ lesson11/code.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <error.h>
+#include <unistd.h>
 int main(int argc,char * argv[],char *env[])
 {
 
@@ -10,17 +11,24 @@ int main(int argc,char * argv[],char *env[])
 	if(fp==NULL)
 	{
 		error(0,errno,"fopen error");
+		perror("perror: fopen error:");
+		//printf("\n");
+		printf("strerror: fopen error:%s\n",strerror(errno));
 	}
 	else
 	{
-		printf("fopen success");
+		printf("fopen success\n");
+		fclose(fp);
 	}
+	
 
 	//putenv("zhuruncai=100");
 	//printf("zhuruncai:%s\n",getenv("zhuruncai"));
 	//for(int i=0;env[i];i++)
 	//{
 	//	printf("%s\n",env[i]);
+	//
 	//}
+	_exit(10);
 	return 0;
 }
