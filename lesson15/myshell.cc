eeca1ee85a76749b30743a9ee84ae01affc43ef3
是回忆加了分 <2144846840@qq.com>
2024-10-20 16:33:57 +0800

    shell

--- lesson15/myshell.cc
+++ lesson15/myshell.cc
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cstdlib>
 #include <string>
 #include <cstring>
 #include <iostream>
@@ -10,12 +11,27 @@
 
 
 using namespace std;
+extern char ** environ;
 const int basesize=1024;
-
+const int envnum=64;
 const int argvnum=64;
+const int existenvnum=0;
+
+
+//命令行参数
 char * gargv[argvnum];
 int gargc=0;
 const char * sep=" ";
+
+
+//全局的工作路径
+char pwdenv[basesize];//用来更新环境变量
+char pwd[basesize];//用来保存当前的工作路径
+
+//环境变量
+char * genv[envnum];
+
+
 
 string GetUserName()
 {
@@ -26,15 +42,46 @@ string GetUserName()
 string GetHostName()
 {
 	string hostname=getenv("HOSTNAME");
-	return hostname.empty()?"None":name;
+	return hostname.empty()?"None":hostname;
+
 }
 
 string GetPwdName()
 {
-	string pwd =getenv("PWD");
-	int pos=pwd.find_last_of("/");
-	pwd=pwd.substr(pos+1,pwd.size()-pos);
-	return pwd.empty()?"None":pwd;
+	//我们的环境变量具有全局性，需要自己维护
+	//如果chdir修改了当前工作路径，环境变量不会自动更新，需要自己去维护
+	//系统自带的cd会自动更新
+	//我们这里修改环境变量太麻烦，换一种方式getcwd
+	//string pwd =getenv("PWD");
+	//int pos=pwd.find_last_of("/");
+	//pwd=pwd.substr(pos+1,pwd.size()-pos);
+	//return pwd.empty()?"None":pwd;
+	
+	//getcwd函数
+	//这里的方法是，定义一个全局的pwdenv，每次保存从系统里面得到的cwd
+	//而不是从环境变量里面得到的
+	//chdir修改cwd后，环境变量需要我们自己维护比较麻烦
+	string spwd;
+	if(nullptr== getcwd(pwd,sizeof(pwd)))
+	{
+		return "None";
+	}
+
+
+	//自己维护环境变量
+	snprintf(pwdenv,sizeof(pwdenv),"PWD=%s",pwd);
+	//printf("%s\n",pwdenv);
+	putenv(pwdenv);
+
+	//下面是修改我们得到的工作路径，而不是一长串
+	spwd=pwd;
+	int pos=spwd.find_last_of("/");
+	spwd=spwd.substr(pos+1,spwd.size()-pos);
+	strcpy(pwd,spwd.c_str());
+	return pwd;
+
+
+
 }
 
 string MakeCommandline()
@@ -97,7 +144,62 @@ void debug()
 //cd等等命令，改变的是子进程的cwd不是父进程的
 //我们父进程shell的没有改变
 //这种需要父进程执行的命令叫做内建命令
-//
+
+
+//我们这里采用直接从系统的bash里面拿到我们的环境变量表
+void InitEnv()
+{
+	int index=0;
+	while(environ[index])
+	{
+		//strlen不包括'\0'，所以我们开辟空间时候，要多开辟一个空间，给\0留一个位置
+		//1.创建空间
+		genv[index]=(char *)malloc(sizeof(char)*(strlen(environ[index]+1)));
+		//2.复制
+		strncpy(genv[index],environ[index],strlen(environ[index]+1));
+		++index;
+	}
+	genv[index]=nullptr;
+}
+
+
+void DestroyEnv()
+{
+	for(int i=0;genv[i];++i)
+	{
+		free(genv[i]);
+		genv[i]=nullptr;
+	}
+}
+
+
+
+void AddEnv(const char * newenv)
+{
+	int index=0;
+	while(genv[index])
+		++index;
+	genv[index]=(char* )malloc(sizeof(char)*(strlen(newenv)+1));
+	strncpy(genv[index],newenv,strlen(newenv)+1);
+	++index;
+	genv[index]=nullptr;
+}
+
+void debugenviron()
+{
+	for(int i=0;environ[i];++i)
+	{
+		printf("environ[%d]:%s\n",i,environ[i]);
+	}
+}
+void debugenv()
+{
+	for(int i=0;genv[i];++i)
+	{
+		printf("env[%d]:%s\n",i,genv[i]);
+	}
+}
+
 bool ExecuteCommandLine()
 {
 	pid_t id=fork();
@@ -125,12 +227,32 @@ bool ExecuteCommandLine()
 
 bool CheckAndExecBulidCommand()
 {
+	//1.cd命令修改当前的工作目录，不能交给子进程处理
 	if(strcmp(gargv[0],"cd")==0)
 	{
-		if(gargc>=2)
+		if(gargc==2)
 		{
 			chdir(gargv[1]);
 
+		}
+		return true;
+	}
+	//2.这种export有关环境变量的指令，我们要单独维护自己的环境变量
+	else if (strcmp(gargv[0],"export")==0)
+	{
+		if(gargc==2)
+		{	
+			AddEnv(gargv[1]);		
+
+		}	
+		return true;
+	}
+	else if(strcmp(gargv[0],"env")==0)
+	{
+		for(int i=0;genv[i];i++)
+		{
+			printf("%s\n",genv[i]);	
+		
 		}
 		return true;
 	}
@@ -139,8 +261,13 @@ bool CheckAndExecBulidCommand()
 int main()
 {
 	char  commandbuffer[basesize];
+
+	InitEnv();
+	//debugenviron();
+	//debugenv();
 	while(true)
 	{
+		
 		//sleep(1);
 		//1.打印命令行
 		//这里我们想法是用环境变量里面的数据
@@ -166,12 +293,24 @@ int main()
 		//	}
 
 		//检查内建命令
+		//有些命令比如ls等等命令，我们可以让子进程去执行
+		//但是有些命令，比如:cd,export,echo等等命令属于内部命令
+		//这些命令是直接修改父进程，也就是shell的属性
+		//如果这些命令交给子进程去执行，那么只会更改子进程的属性，改不了当前父进程也就是shell的属性
+		//如此一来，达不到我们的要求，我们需要对这些内建命令单独处理
 		if(CheckAndExecBulidCommand())
 			continue;
+
+
+		
 		//4.执行命令行
+		//我们现在运行的就是shell外壳，我们不能让我们自己的进程去执行命令
+		//如果自己去执行命令，那么如果出问题，shell外壳直接就挂掉了
+		//所以，我们使用多进程，使用子进程去执行命令，
+		//子进程发生进程替换后，后找到对应的执行程序执行命令
 		ExecuteCommandLine();
 
 	}
-	
+	DestroyEnv();	
 	return 0;
 }
