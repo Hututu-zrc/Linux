b0eef4a76e1b970a507bd2b575c3265694734e08
memories-added-points <2144846840@qq.com>
2024-11-12 20:35:08 +0800

    processpool

--- lesson19/processpool/Task.hpp
+++ lesson19/processpool/Task.hpp
@@ -42,14 +42,14 @@ public:
         InsertTask(Upload);
         InsertTask(Log);
     }
-    void InsertTask(task_t t)
+    void InsertTask(task_t t) // 将我们的任务插入到我们的TaskManage类里面
     {
         _tasks[number++] = t;
     }
-   
-    int SelectTask()
+
+    int SelectTask() // 随机挑选任务，返回任务码
     {
-        return rand()%number;
+        return rand() % number;
     }
     void ExecuteTask(int taskcode)
     {
@@ -62,12 +62,35 @@ public:
             std::cerr << "taskcode error" << endl;
         }
     }
-    ~TaskManage()
-    {
-    }
+    ~TaskManage() {}
 
 private:
     std::unordered_map<int, task_t> _tasks;
 };
 
-TaskManage tm;
+TaskManage tm; // 创建一个全局的类对象，方便调用
+
+void Worker()
+{
+    // read->0
+    // 进程里面的每个进程都在从我们的管道里面读数据
+    // 父进程写入数据后，子进程就是管道里面读，看哪个读的到，就输出
+    while (true)
+    {
+        int taskcode = 0;
+
+        ssize_t n = ::read(0, &taskcode, sizeof(taskcode));
+        if (n == sizeof(taskcode))
+        {
+            tm.ExecuteTask(taskcode);
+        }
+        else if (n == 0)
+        {
+            break;
+        }
+        else
+        {
+        }
+        // sleep(1);
+    }
+}
