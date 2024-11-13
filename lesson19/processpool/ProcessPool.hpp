4ad065209663de2a19b6f0071bcbc5d4f793eab4
memories-added-points <2144846840@qq.com>
2024-11-13 21:10:26 +0800

    processpool

--- lesson19/processpool/ProcessPool.hpp
+++ lesson19/processpool/ProcessPool.hpp
@@ -61,6 +61,23 @@ public:
             // 子进程用来读
             if (id == 0)
             {
+                // 我们使用vector存储的管道，每个子进程开辟之前，
+                //(注意，我们现在准备修改的是子进程的文件描述符表)如果里面存储有管道，就遍历每一个管道，将管道里面的写端全部关闭掉
+                // 假设前面管道的写端是6，父进程的文件描述符表和子进程的文件描述表里面，该管道的写端都是6号
+                // 但是我们现在是子进程的代码段，使用的子进程的数据段的拷贝副本，所以，这里关掉的是子进程的文件描述符表里面的写端，不会影响到父进程
+
+                // std::cout << getpid() << ", child close history fd: ";
+                if (_channels.size() != 0)
+                {
+                    // 这里面的for循环，i<_channels.size()就可以了，不需要减1
+                    for (size_t i = 0; i < _channels.size(); i++)
+                    {
+                        // std::cout << _channels[i].GetWid() << " ";
+                        _channels[i].Close();
+                    }
+                }
+                // std::cout << " over" << std::endl;
+
                 ::close(pfds[1]);
 
                 // 改变文件描述符，使我们的Work读取命令的时候，直接从stdin里面读取
@@ -80,8 +97,8 @@ public:
     {
         int taskcode = 0;
         int who = 0;
-        
-        int cnt=10;
+
+        int cnt = 10;
         while (cnt--)
         {
 
@@ -95,18 +112,20 @@ public:
             // c、分发任务
             chl.SendTask(taskcode);
             cout << chl.GetName() << endl;
-            sleep(2);
+            sleep(1);
         }
     }
 
     void ClearProcessPool()
     {
+
+        // version_3
+        // 如果我们一定要从前往后关闭的话，也有方法
+        // 详细请看上面的子进程的代码块
+
         for (auto &c : _channels)
         {
             c.Close();
-        }
-        for (auto &c : _channels)
-        {
             int status = 0;
             int rid = ::waitpid(c.GetSubPid(), &status, 0);
             if (rid < 0)
@@ -114,8 +133,54 @@ public:
                 std::cerr << "waitpid" << std::endl;
                 exit(Wait_Err);
             }
-            cout<<"sub process "<<rid<< " exit success"<<endl;
+            cout << "sub process " << rid << " exit success" << endl;
         }
+
+        // //version_2
+        // //这里会产生问题，程序会一直阻塞式等待
+        // //原因分析：我们创建管道的时候，主程序关掉读端，子进程关掉写端
+        // //但是多个管道的时候，主进程分配的还是3号读端，和x号写端
+        // //然后子进程拷贝为副本的时候，上个管道的写端被拷贝下来了，每创建一个管道就写端就会重复
+        // //然后我们从第一个管道开始关闭写端的时候，同一管道有多个写端，关闭一个没有用，程序就会一直阻塞式等待
+        // //解决方法一就是从最后一个管道开始关闭掉写端，反着向前关闭
+        // //当我们开始关闭掉最后一个管道的写端的时候，对应的子进程的文件描述符表里面有上一个管道的写端
+        // //但是，我们关闭掉最后一个管道的写端，对应的子进程也就被回收了，文件描述符表也就没有了
+        // //所以，上一个管道的写端就少了一个。
+        // //我们从后往前关闭，就不会因为管道的多写端而产生问题，第一个管道的写端是最多的
+
+        // //这个地方还不能使用size_t,因为最后减成-1的时候，会变成很大很大的数
+        // for (int i=_channels.size()-1;i>=0;--i)
+        // {
+        //     _channels[i].Close();
+        //     int status = 0;
+        //     int rid = ::waitpid(_channels[i].GetSubPid(), &status, 0);
+        //     if (rid < 0)
+        //     {
+        //         cout<<"i:"<<i<<endl;
+        //         std::cerr << "waitpid" << std::endl;
+        //         exit(Wait_Err);
+        //     }
+        //     cout << "sub process " << rid << " exit success" << endl;
+        // }
+
+        // //version_1
+        // //这里我们清理管道写端的时候，使用了两个for循环分开清理
+        // //这里不会产生问题
+        // for (auto &c : _channels)
+        // {
+        //     c.Close();
+        // }
+        // for (auto &c : _channels)
+        // {
+        //     int status = 0;
+        //     int rid = ::waitpid(c.GetSubPid(), &status, 0);
+        //     if (rid < 0)
+        //     {
+        //         std::cerr << "waitpid" << std::endl;
+        //         exit(Wait_Err);
+        //     }
+        //     cout<<"sub process "<<rid<< " exit success"<<endl;
+        // }
     }
 
 private:
