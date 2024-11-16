6470c34b52ffb35a236d6ca8036e7df4812ff415
memories-added-points <2144846840@qq.com>
2024-11-16 17:08:05 +0800

    sharememory

--- lesson20/SharedMemory/Server.cc
+++ lesson20/SharedMemory/Server.cc
@@ -17,15 +17,36 @@ int main()
     // IPC_EXCL保证，不存在就创建，存在就保存
 
     // IPC_CREAT + IPC_EXCL 功能就是，保证我们开辟的共享内存是新的
-    int shmid = ::shmget(k, gsize, IPC_CREAT | IPC_EXCL);
+    //这里我们还要有gmode，也就是进程对于共享内存的权限必要也要说明
+    int shmid = ::shmget(k, gsize, IPC_CREAT | IPC_EXCL | gmode);
     if (shmid < 0)
     {
         std::cerr << "shmget" << std::endl;
     }
     cout << "shmid: " << To_HEX(shmid) << endl;
 
-    int ret = ::shmctl(shmid, IPC_RMID, nullptr);
-    if (ret < 0)
+
+    sleep(1);
+    // 3、将开辟的共享内存关联到我们的程序里面
+    void *shmaddr = ::shmat(shmid, nullptr, 0);
+    if ((long long)shmaddr < 0)
+    {
+        std::cerr << "shmat" << endl;
+    }
+    cout<<"shmaddr :"<<(void*)shmaddr<<endl;
+    sleep(1);
+
+
+    // 4、将共享内存脱离当前的进程段
+    int ret_shmdt = shmdt(shmaddr);
+    if (ret_shmdt < 0)
+    {
+        std::cerr << "shmdt" << endl;
+    }
+
+    // 5、将我们的共享内存标识为销毁段
+    int ret_shmctl = ::shmctl(shmid, IPC_RMID, nullptr);
+    if (ret_shmctl < 0)
     {
         std::cerr << "shmctl" << endl;
     }
