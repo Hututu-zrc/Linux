b711a21c368319fcc4967fc1d15bf417531d5577
memories-added-points <2144846840@qq.com>
2024-11-30 11:24:48 +0800

    fopen

---
+++ lesson16/fopen/myfile.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    FILE *fpw = fopen("bite.txt", "w");
+    if (fpw == NULL)
+    {
+        perror("fopen");
+        return 1;
+    }
+    char *strw = "linux so easy!";
+    fwrite(strw, sizeof(char), strlen(strw), fpw);
+    fclose(fpw);
+    FILE *fpr = fopen("bite.txt", "r");
+    if (fpr == NULL)
+    {
+        perror("fopen");
+        return 1;
+    }
+    char strr[20];
+    size_t ret=fread(strr, sizeof(char), sizeof(strr)/sizeof(char), fpr);
+    printf("strr len:%lu %s\n",ret,strr);
+    fclose(fpr);
+
+    return 0;
+}
