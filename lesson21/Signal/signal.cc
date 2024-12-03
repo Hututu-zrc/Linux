c9f5a897ae5192bba6051cae0c94bdb8290f4c6e
memories-added-points <2144846840@qq.com>
2024-12-03 16:42:35 +0800

    signal

--- lesson21/Signal/signal.cc
+++ lesson21/Signal/signal.cc
@@ -10,48 +10,56 @@ using std::endl;
 void Handler(int signo)
 {
 
+    cout << "unlock signal" << endl;
+    exit(0);
 }
 int main()
 {
+
+    // 首先将block信号集里面的2号信号进行屏蔽
+    sigset_t bset, obset;
+    ::sigemptyset(&bset);
+    ::sigemptyset(&obset);
+    ::sigaddset(&bset, 2); // 这里只是修改了信号集，没有实际添加到进程当中
+    ::sigprocmask(SIG_SETMASK, &bset, &obset);
+
+    // 打印pending信号集
     sigset_t pset;
     ::sigemptyset(&pset);
-    int n=::sigpending(&pset);
-    if(n<0)
+
+    signal(2, Handler);
+    int cnt = 10;
+    while (true)
     {
-        cerr<<"sigpending"<<endl;
-        exit(1);
-    }
-    while(true)
-    {
-        cout<<"curr pending list [" <<getpid()<<"]:";
-        for(int i=1;i<=31;i++)
+        // 这里的sigpending要放到循环里面，不然一直打印的都是之前的pending
+        int n = ::sigpending(&pset);
+        if (n < 0)
         {
-            int ret=::sigismember(&pset,i);
-            if(ret==1)
-                cout<<1;
-            else if(ret==0)
-                cout<<0;
-            else
-                cerr<<"sigismember"<<endl;
+            cerr << "sigpending" << endl;
+            exit(1);
         }
-        cout<<endl;
+        cout << "curr pending list [" << getpid() << "]:";
+        for (int i = 1; i <= 31; i++)
+        {
+            // sigismember函数传入的信号的编号，不是信号位图的下标
+            int ret = ::sigismember(&pset, i);
+            if (ret == 1)
+                cout << 1;
+            else if (ret == 0)
+                cout << 0;
+            else
+                cerr << "sigismember" << endl;
+        }
+        cout << endl;
         sleep(1);
+        
+        if((cnt--)==0)
+        {
+            sigprocmask(SIG_SETMASK, &obset, nullptr);
+        }
     }
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // void Handler(int signo)
 // {
