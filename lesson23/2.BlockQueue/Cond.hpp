3237410a66b2d148ff1150ebf3d63db48dbdc264
memories-added-points <2144846840@qq.com>
2025-02-17 18:35:42 +0800

    BlockQueue

---
+++ lesson23/2.BlockQueue/Cond.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <pthread.h>
+#include <unistd.h>
+#include "Mutex.hpp"
+#include "Cond.hpp"
+namespace CondModule
+{
+    class Cond
+    {
+    public:
+        Cond()
+        {
+            pthread_cond_init(&_cond,nullptr);
+        }
+        void Wait(MutexMudole:: Mutex &mutex)
+        {
+            pthread_cond_wait(&_cond,mutex.LockPtr());
+        }
+        void Notify()
+        {
+            pthread_cond_signal(&_cond);
+        }
+        void NotifyAll()
+        {
+            pthread_cond_broadcast(&_cond);
+        }
+        ~Cond()
+        {
+            pthread_cond_destroy(&_cond);
+        }
+    private:
+        pthread_cond_t _cond;
+    };
+}
