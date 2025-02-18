680bc1b60eb226f85405eebef5eb1cab4223ff12
memories-added-points <2144846840@qq.com>
2025-02-18 20:08:11 +0800

    log

---
+++ lesson24/1.Log/main.cc
@@ -0,0 +1,12 @@
+#include "Log.hpp"
+
+
+using namespace LogModule;
+int main()
+{
+    LOG(LogLevel::DEBUG)<<"hello zrc";
+    LOG(LogLevel::DEBUG)<<"hello zrc";
+    LOG(LogLevel::DEBUG)<<"hello zrc";
+    LOG(LogLevel::DEBUG)<<"hello zrc";
+    return 0;
+}
