448d554743ee0dadd4a5230829a6771cf8d3b418
memories-added-points <2144846840@qq.com>
2025-03-01 17:52:16 +0800

    tcp_server

--- lesson26/1.Echo_Server/TcpServerMain.cc
+++ lesson26/1.Echo_Server/TcpServerMain.cc
@@ -1,10 +1,17 @@
 #include "TcpServer.hpp"
+
 #include <memory>
 #include <iostream>
 int main()
 {
-    std::unique_ptr<TcpServer> server=std::make_unique<TcpServer>();
+    std::unique_ptr<Command> cmd = std::make_unique<Command>();
+    // unique_ptr不能被复制，所以lambda表达式不能复制捕获
+    //这里的lambda表达式相当于多套了一层，和handle_t函数指针使用一样的参数，返回同样的参数
+    //内部再次调用Command类里面的Execute函数
+    std::unique_ptr<TcpServer> server = std::make_unique<TcpServer>([&cmd](const std::string & cmdstr)
+                                                                    { return cmd->Execute(cmdstr); });
     server->InitServer();
     server->Start();
+    server->Stop();
     return 0;
 }
