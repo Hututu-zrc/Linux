9183eef49c3ac3aa96005c8bdcf19c68643c8ee3
memories-added-points <2144846840@qq.com>
2025-03-03 16:22:35 +0800

    netcal

---
+++ lesson27/NetCal/TcpServer.hpp
@@ -0,0 +1,233 @@
+#pragma once
+
+#include <sys/types.h> /* See NOTES */
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <string.h>
+#include <errno.h>
+#include <functional>
+#include <iostream>
+
+#include "Log.hpp"
+#include "Common.hpp"
+#include "InetAddr.hpp"
+#include "ThreadPool.hpp"
+#include "Command.hpp"
+
+#define BACKLOG 5
+using namespace LogModule;
+using namespace ThreadPoolModule;
+// handler_t 从client接收命令，然后解析，返回结构
+using handler_t = std::function<std::string(std::string)>;
+static const int gport = 8888;
+
+class TcpServer
+{
+    struct Data
+    {
+        int *sockfd;
+        TcpServer *self;
+    };
+
+public:
+    // using func_t = std::function<void *(void *)>;
+    using task_t = std::function<void()>;
+
+    TcpServer(handler_t handler, int port = gport)
+        : _listensockfd(-1),
+          _port(port),
+          _addr(_port),
+          _isrunning(false),
+          _handler(handler)
+
+    {
+    }
+    void InitServer()
+    {
+        _listensockfd = ::socket(AF_INET, SOCK_STREAM, 0);
+        if (_listensockfd < 0)
+        {
+            LOG(LogLevel::FATAL) << strerror(errno);
+            Die(SOCKET_ERR);
+        }
+
+        int n = ::bind(_listensockfd, CONV(&_addr), sizeof(_addr));
+        if (n < 0)
+        {
+            LOG(LogLevel::FATAL) << strerror(errno);
+            Die(BIND_ERR);
+        }
+
+        // 由于TCP要先建立连接，如果多个client发送信息的话，需要排队等待
+        int m = ::listen(_listensockfd, BACKLOG);
+        if (m < 0)
+        {
+            LOG(LogLevel::FATAL) << strerror(errno);
+            Die(LISTEN_ERR);
+        }
+        LOG(LogLevel::INFO) << "listen is sucess,_listensockfd: " << _listensockfd;
+    }
+    void HandlerRequest(int sockfd)
+    {
+        LOG(LogLevel::INFO) << "HandlerRequest is sucess,sockfd: " << sockfd;
+
+        char buff[1024];
+        // 长任务
+        while (true)
+        {
+
+            memset(buff, 0, sizeof(buff));
+
+            // 网络当中还有另一种读写的方式 recv && send接口
+            //  int n = ::read(sockfd, buff, sizeof(buff) - 1);//read不完善，eg：发了10byte，但是只读了5byte
+
+            ssize_t n = ::recv(sockfd, buff, sizeof(buff) - 1, 0);
+            // LOG(LogLevel::INFO) << "buff: " << buff;
+            //  read读不到数据的时候，返回-1，所以这里死循环
+            if (n > 0)
+            {
+                buff[n] = 0;
+                // std::string echomessage("Echo# ");
+                // echomessage += buff;
+
+                // 这里读到数据以后，交给command处理
+                //对命令进行限制，只允许指定命令进入到_handler
+                //放到Command里面去，不要放到服务器上面判断
+                std::string ret = _handler(buff);
+                // ::write(sockfd, echomessage.c_str(), echomessage.size());//写入也是不完善的
+                ::send(sockfd, ret.c_str(), ret.size(), 0); // 写入也是不完善的
+            }
+            else if (n == 0)
+            {
+                LOG(LogLevel::INFO) << "Client exit";
+                break;
+            }
+            else
+            {
+                break;
+            }
+        }
+        ::close(sockfd);
+        // LOG(LogLevel::DEBUG) << "sockfd close,sockfd: " << sockfd;
+    }
+
+    // 这里出现的问题就是，静态类内函数没有this指针
+    // 解决方法就是：传递strcut结构体，然后结构体里面放个指针
+
+    static void *Handler(void *args)
+    {
+        // 线程内部设置为分离状态，免得回收
+        pthread_detach(pthread_self());
+
+        Data *data = static_cast<Data *>(args);
+        data->self->HandlerRequest(*data->sockfd);
+        return nullptr;
+    }
+    void Start()
+    {
+        if (_isrunning)
+            return;
+        _isrunning = true;
+        while (true)
+        {
+            // int accept(int sockfd, struct sockaddr *addr,socklen_t *addrlen, int flags);
+            struct sockaddr_in peer;
+
+            socklen_t peerlen = sizeof(peer); // 初始化未peer的大小
+
+            // 我们获取client的信息：数据（sockfd）+client socket信息（accept || recvfrom）
+            int sockfd = accept(_listensockfd, CONV(&peer), &peerlen);
+            if (sockfd < 0)
+            {
+                LOG(LogLevel::FATAL) << strerror(errno);
+
+                continue;
+            }
+            LOG(LogLevel::INFO) << "accept is sucess,sockfd: " << sockfd;
+
+            // //version_0 版本  无法同时处理多个客户端
+            // HandlerRequest(sockfd);
+
+            // // version_1 版本 多进程版本
+            // // 父子进程使用不同的文件描述符表，子进程会拷贝文件描述符表
+            // // 解决问题1：关闭不需要的文件描述符表  //已解决
+            // // 解决问题2：HandlerRequest里面是死循环收发信息，
+            // // 如果不退出，父进程阻塞等待，还是只能处理一个客户端
+            // // 解决方法1：signal(SIGCHLD,SIG_IGN);
+            // // 当父进程调用 signal(SIGCHLD, SIG_IGN); 后，内核会在子进程终止时自动回收其资源，
+            // // 而不需要父进程调用 wait() 或 waitpid() 来处理，从而避免了僵尸进程的产生。
+            // //解决方法2：采用子进程里面再次开辟进程，使用孙子进程去执行任务，让子进程退出，
+            // //这样孤儿进程就可以直接被回收，不需要父进程等待了
+            // // 子进程去执行任务
+            // signal(SIGCHLD, SIG_IGN);
+            // int id = fork();
+            // if (id < 0)
+            // {
+            //     LOG(LogLevel::FATAL) << strerror(errno);
+            //     break;
+            // }
+            // else if (id == 0)
+            // {
+            //     close(_listensockfd);
+            //     LOG(LogLevel::INFO) << "SubProcess success";
+            //     if(fork()==0)
+            //     {
+            //         HandlerRequest(sockfd);
+            //     }
+            //     exit(0);
+            // }
+
+            // close(sockfd);
+            // int rid = ::waitpid(id, nullptr, 0);
+            // if (rid < 0)
+            // {
+            //     LOG(LogLevel::FATAL) << "waitpid error";
+            //     break;
+            // }
+
+            // // version_3 多线程版本
+            // pthread_t tid;
+            // // 因为线程之间是共享同一份文件描述符表的，所以这里使用的时候必须拷贝一份sockfd
+            // // 不然多线程使用的时候，前面的线程还在使用，后面的新线程直接修改了sockfd
+            // //这里和系统的分配策略有关系，关闭了的文件描述符不会立即启用
+            // int *psockfd = new int(sockfd);
+            // Data data;
+            // data.self = this;
+            // data.sockfd = psockfd;
+            // // 这里的错误点就是Handler是类内函数，需要传递指针，否则就是改成静态类内成员函数
+            // pthread_create(&tid, nullptr, Handler, &data);
+
+            // version_4 线程池版本
+            // 这里的sockfd是赋值拷贝
+            // 线程池只适合短任务，不适合长任务
+
+            Threadpool<task_t>::CreateSingleThreadPool()->Equeue([this, sockfd]()
+                                                                 {
+               // LOG(LogLevel::DEBUG)<<"lambda ";
+                this->HandlerRequest(sockfd); });
+        }
+        _isrunning = false;
+    }
+    void Stop()
+    {
+        _isrunning = false;
+        Threadpool<task_t>::CreateSingleThreadPool()->Stop();
+        Threadpool<task_t>::CreateSingleThreadPool()->Wait();
+    }
+    ~TcpServer() {}
+
+private:
+    // 进程的文件描述符是有限的，所以不需要的时候记得关闭
+    // Linux支持对文件描述符进行扩展的，ulimit -a 最多扩展到64kb个，核心就是结构体里面存在动态数组，可以扩展
+
+    int _listensockfd;
+    int _port; // 用户输入的端口号
+
+    Inet_addr _addr; // 服务器内部的属性
+    bool _isrunning; // 判断服务器是否启动
+
+    // 处理上层任务的入口
+    handler_t _handler;
+};
---
+++ lesson27/NetCal/ThreadPool.hpp
@@ -0,0 +1,189 @@
+#pragma once
+#include <pthread.h>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+#include <queue>
+#include <memory>
+#include "Mutex.hpp"
+#include "Cond.hpp"
+#include "Thread.hpp"
+#include "Log.hpp"
+using namespace MutexModule;
+using namespace LogModule;
+using namespace CondModule;
+using namespace ThreadModule;
+
+namespace ThreadPoolModule
+{
+    const static int defaultnum = 10;
+
+    template <typename T>
+    class Threadpool
+    {
+
+    private:
+        bool IsEmpty()
+        {
+            return _tasks.empty();
+        }
+        void ExecuteTask(std::string name)
+        {
+
+            while (true)
+            {
+                // 1.拿到任务
+                // 访问了临界资源任务队列，需要加锁
+                sleep(1);
+                T t;
+
+                // 这里的锁在{}当中，出了这个作用域就会被销毁了
+                {
+                    LockGuard lock(_mutex);
+
+                    // 任务队列不为空 && 线程池处于进行状态
+                    // 此时线程池正常运行
+                    while (IsEmpty() && _isrunning)
+                    {
+
+                        _wait_num++;
+                        _cond.Wait(_mutex);
+                        _wait_num--;
+                    }
+
+                    if (IsEmpty() && !_isrunning)
+                    {
+                        LOG(LogLevel::DEBUG) << "threadpool closed";
+                        break;
+                    }
+
+                    // 如果线程池为空 && 线程池处于关闭状态，此时跳出循环
+                    // 准备回收线程
+
+                    LOG(LogLevel::DEBUG) << name << " is running";
+
+                    t = _tasks.front();
+                    _tasks.pop();
+                }
+                // 2.处理任务
+                // 处理任务自己处理就好了，不需要再加锁期间处理
+                t();
+            }
+        }
+
+        // 单例模式不允许赋值和拷贝
+        Threadpool(const Threadpool<T> &pool) = delete;
+        Threadpool<T> &operator=(const Threadpool<T> &pool) = delete;
+
+        // 单例模式还是需要构造函数初始化的
+        Threadpool(int num = defaultnum) : _thread_num(num),
+                                           _wait_num(0),
+                                           _isrunning(false)
+        {
+            for (int i = 0; i < _thread_num; ++i)
+            {
+                _threads.push_back(std::make_shared<Thread>(std::bind(&Threadpool::ExecuteTask, this, std::placeholders::_1)));
+            }
+        }
+
+    public:
+        static Threadpool<T> *CreateSingleThreadPool()
+        {
+            if (_instance == nullptr)
+            {
+                // 非静态变量是在函数执行期间在栈上分配内存的局部变量，其生命周期仅限于函数的执行期间。
+                // 静态成员函数可以像普通函数一样在其内部创建和使用局部变量，包括非静态变量，因为这些变量的创建和使用并不依赖于对象的实例。
+                // 核心就是静态成员函数里面变量和函数都不依赖this指针就行
+                if (_instance == nullptr)
+                {
+                    LockGuard lockguard(_lock);
+                    if(_instance==nullptr)
+                    {
+                        _instance=new Threadpool<T> ();
+                        _instance->Start();
+                    }
+                }
+            }
+            return _instance;
+        }
+        void Equeue(const T&t) // 任务进入队列的函数
+        {
+            // 这个地方要访问临界资源，所以要加锁保护
+            LockGuard lock(_mutex);
+            if (_isrunning == false)
+                return;
+            _tasks.push(move(t));
+
+            if (_wait_num > 0)
+            {
+                _cond.Notify();
+            }
+        }
+        void Start() // 线程池启动函数
+        {
+            LockGuard lock(_mutex);
+
+            if (_isrunning)
+                return;
+            // 这里如果不加锁 && _isrunning 放到最后的话，就会产生问题
+            // 因为线程是并发跑的，可能前一个线程刚启动的时候，就直接执行ExecuteTask
+            // 但是ExecuteTask里面有if (IsEmpty() && !_isrunning) 就直接退出掉这个进程了
+            // 解决办法：1、加锁  2、将_isrunning=true 放到if (_isrunning)后面
+            _isrunning = true;
+
+            for (auto &e : _threads)
+            {
+                e->Start();
+                LOG(LogLevel::DEBUG) << e->GetName() << " is start";
+            }
+        }
+        void Wait() // 线程池等待函数
+        {
+            for (auto &e : _threads)
+            {
+                e->Join();
+                LOG(LogLevel::DEBUG) << e->GetName() << " is stop";
+            }
+        }
+        void Stop() // 线程池停止函数
+        {
+            // 线程池停止我们必须保证
+            // 1.任务队列里面的任务全部都进行完了
+            // 2.将所有的休眠进程都唤醒
+
+            LockGuard lock(_mutex);
+
+            if (!_isrunning) // 关闭状态就退出
+            {
+                return;
+            }
+            _isrunning = false;
+            if (_wait_num > 0)
+            {
+                _cond.Notify();
+            }
+        }
+        ~Threadpool()
+        {
+        }
+
+    private:
+        int _thread_num;                               // 线程池里面的线程的个数
+        int _wait_num;                                 // 有多少个线程在等待
+        bool _isrunning;                               // 线程池的启动状态，默认是关闭
+        std::vector<std::shared_ptr<Thread>> _threads; // 线程存放的地方
+
+        // 判断一个地方要不要使用锁，核心就是盖地方要不要访问临界资源
+        std::queue<T> _tasks; // 任务队列 临界资源（多线程访问）
+        Mutex _mutex;
+        Cond _cond;
+        static Threadpool<T> *_instance;
+        static Mutex _lock; // 用来保护单例的静态锁
+    };
+
+    template <typename T>
+    Threadpool<T> *Threadpool<T>::_instance = nullptr;
+
+    template <typename t>
+    Mutex Threadpool<t>::_lock; // 定义一个全局的锁来保护单例
+}
